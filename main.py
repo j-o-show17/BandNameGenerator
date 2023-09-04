834f5f45d7f3e9d79d1366c7d8334ba8cbd63c69
j.o.s <joshua.olasunkanmi.sorungbe@gmail.com>
2023-09-04 17:36:40 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Greetings
+print("\n************* Welcome to Band Name Generator! ****************")
+
+#Prompt city
+city = input("\nWhich city did you grow up? ")
+
+#Promt pet
+pet = input("\nWhat is the name of your pet? ")
+
+bandName = city + " " + pet
+
+ #output band name
+
+print("\nYour band name is: " + bandName +"\n")
